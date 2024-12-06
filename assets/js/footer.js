ff6dff3222add0669bd5faf9e9ba9dd0ec73cc93
SergioDeLaHoz16 <sddelahoz@unicesar.edu.co>
2024-12-06 14:54:28 -0500

    fix: footer and header in pages

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,16 +1,25 @@
 function loadFooter() {
-    const baseUrl = window.location.origin + window.location.pathname.replace(/\/$/, '');
-    fetch(`${baseUrl}/assets/html/footer.html`)
+    // Construye la URL base para acceder al archivo del footer
+    const baseUrl = window.location.origin;
+    const footerUrl = `${baseUrl}/assets/html/footer.html`;
+
+    console.log('Fetching footer from:', footerUrl);
+
+    // Realiza la solicitud para obtener el archivo footer.html
+    fetch(footerUrl)
         .then(response => {
+            console.log('Footer response:', response);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.text();
         })
         .then(data => {
-            const headerElement = document.getElementById('footer');
-            if (headerElement) {
-                headerElement.innerHTML = data;
+            // Inserta el contenido del footer en el elemento correspondiente
+            const footerElement = document.getElementById('footer');
+            if (footerElement) {
+                footerElement.innerHTML = data;
+                console.log('Footer loaded successfully.');
             } else {
                 console.error("Error: Element with ID 'footer' not found.");
             }
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,23 +1,21 @@
 function loadHeader() {
-    const baseUrl = window.location.origin + window.location.pathname.replace(/\/$/, '');
-    console.log('Base URL:', baseUrl);
+    const baseUrl = window.location.origin;
     const headerUrl = `${baseUrl}/assets/html/header.html`;
     console.log('Fetching header from:', headerUrl);
 
     fetch(headerUrl)
         .then(response => {
-            console.log('Response:', response);
+            console.log('Header response:', response);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.text();
         })
         .then(data => {
-            console.log('Header content:', data);
-            const headerElement = document.getElementById('header');    
+            const headerElement = document.getElementById('header');
             if (headerElement) {
                 headerElement.innerHTML = data;
-                console.log('Header successfully loaded.');
+                console.log('Header loaded successfully.');
             } else {
                 console.error("Error: Element with ID 'header' not found.");
             }
